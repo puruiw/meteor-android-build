7ecda8ee0ca6c35ad839a0cf42e65e9d5be8882d
purui <puruiw@yahoo.com>
2019-04-25 22:35:08 -0700

    Rename script.

--- build-app.sh
+++ build-app.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 # Usage:
-#   ./build-smart-app.sh <app_dir> <server_address>
+#   ./build-app.sh <app_dir> <server_address>
 docker build . -t meteor-android-builder:latest
 docker docker run --rm -it -e APP_SERVER=$2 -v $1:/app -v $(pwd):/build meteor-android-builder:latest
